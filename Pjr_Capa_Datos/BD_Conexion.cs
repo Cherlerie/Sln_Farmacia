c2a1a0b5129c50ffad6047c998f7f38e94be42f9
Cherlerie <genesismmejia3@gmail.com>
2025-01-07 06:34:34 -0400

    modificando conexion y creando clase BD_Conexion

---
+++ Pjr_Capa_Datos/BD_Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pjr_Capa_Datos
+{
+    internal class BD_Conexion
+    {
+    }
+}
